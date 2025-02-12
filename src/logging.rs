36559d623c6bbd5766e13efb58469aa7762ea930
Marco Cadetg <marco.cadetg@swisstxt.ch>
2025-02-12 18:10:19 +0100

    feat: add missing logging.rs

---
+++ src/logging.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
+
+pub fn init_logging() -> Result<()> {
+    // Create a default env filter that can be overridden by RUST_LOG
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info,ffmpeg_monitor=debug"));
+
+    // Initialize subscriber with stdout logging
+    tracing_subscriber::fmt()
+        .with_env_filter(env_filter)
+        .with_target(true)
+        .with_thread_ids(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+
+    Ok(())
+}
